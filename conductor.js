09925848e540ddfe75539f625319c744638fe7c2
Steven Borrie <saborrie@gmail.com>
2020-02-19 11:53:41 +0000

    updated conductor to have a default config name

--- conductor.js
+++ conductor.js
@@ -7,27 +7,21 @@ const program = require("commander");
 const server = require("./server");
 
 const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
 
 const handleError = error => {
   console.log(colors.red(error));
   process.exit(1);
 };
 
-const showHelp = () => {
-  program.outputHelp(colors.red);
-  process.exit(1);
-};
-
 program
   .version(require("./package.json").version)
-  .usage("[options] <config file>")
-  .arguments("<config file>")
+  .usage("[options] <config file (defaults to ./conductor.json)>")
+  .arguments("<config file>", "The config file, defaults to ./conductor.json")
   .option("-p, --port <port>", "The port to run the server on, if not set will be randomly selected.")
   .action(async configFile => {
     try {
       const port = program.port;
-      const configString = await readFile(configFile, { encoding: "utf8" }).catch(handleError);
+      const configString = await readFile(configFile || "./conductor.json", { encoding: "utf8" }).catch(handleError);
       const config = JSON.parse(configString);
 
       // start server:
@@ -36,9 +30,5 @@ program
       return handleError(e);
     }
   });
-
-if (!process.argv.slice(2).length) {
-  showHelp();
-}
 
 program.parse(process.argv);
