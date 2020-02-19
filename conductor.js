103ee49f4bcc189a699d79710d76031a7b698440
Steven Borrie <saborrie@gmail.com>
2020-02-19 12:02:03 +0000

    fixed it so that it can run with no args

--- conductor.js
+++ conductor.js
@@ -15,16 +15,14 @@ const handleError = error => {
 
 program
   .version(require("./package.json").version)
-  .usage("[options] <config file (defaults to ./conductor.json)>")
-  .arguments("<config file>", "The config file, defaults to ./conductor.json")
+  .usage("[options] [config file]")
+  .arguments("[config file]", "The config file, defaults to ./conductor.json")
   .option("-p, --port <port>", "The port to run the server on, if not set will be randomly selected.")
   .action(async configFile => {
     try {
       const port = program.port;
       const configString = await readFile(configFile || "./conductor.json", { encoding: "utf8" }).catch(handleError);
       const config = JSON.parse(configString);
-
-      // start server:
       server(config, port);
     } catch (e) {
       return handleError(e);
